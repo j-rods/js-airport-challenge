85d50ea6451542ce78c3180dccbecb251198a82f
emmpak <eiliev05@yahoo.com>
2017-04-11 12:25:45 +0100

    Create plane constructor and add land property

---
+++ spec/PlaneSpec.js
@@ -0,0 +1,9 @@
+describe("Plane", function(){
+  var plane = new Plane();
+
+  describe("land", function() {
+    it('at an airport', function(){
+      expect(plane.land()).toBe(true);
+    });
+  });
+});
